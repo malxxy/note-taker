ed7ddbdfbb70e0f88d4081411aba77e6a6060b57
malxxy <maliayraguen@gmail.com>
2023-03-05 14:31:00 -0800

    Switched route order in server.js to fix get route! successful

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(express.urlencoded({extended:true})); // encode URL
 app.use(express.json()); // parse data froms tring to JSON
 app.use(express.static('public')); // everything that goes to front end is served out of public folder
 
-app.use('/', indexRoutes);
 app.use('/api', notesRoutes);
+app.use('/', indexRoutes);
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`);
